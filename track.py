efd3bfd89fcec979beee0143d40d9ddfc3e5638d
Lee Dain <76834485+manypeople-AI@users.noreply.github.com>
2021-11-18 16:00:58 +0900

    NEWNEW

--- track.py
+++ track.py
@@ -1,11 +1,19 @@
+# limit the number of cpus used by high performance libraries
+import os
+os.environ["OMP_NUM_THREADS"] = "1"
+os.environ["OPENBLAS_NUM_THREADS"] = "1"
+os.environ["MKL_NUM_THREADS"] = "1"
+os.environ["VECLIB_MAXIMUM_THREADS"] = "1"
+os.environ["NUMEXPR_NUM_THREADS"] = "1"
+
 import sys
 sys.path.insert(0, './yolov5')
-# test
-from yolov5.utils.google_utils import attempt_download
+
 from yolov5.models.experimental import attempt_load
+from yolov5.utils.downloads import attempt_download
 from yolov5.utils.datasets import LoadImages, LoadStreams
 from yolov5.utils.general import check_img_size, non_max_suppression, scale_coords, check_imshow, xyxy2xywh
-from yolov5.utils.torch_utils import select_device, time_synchronized
+from yolov5.utils.torch_utils import select_device, time_sync
 from yolov5.utils.plots import plot_one_box
 from deep_sort_pytorch.utils.parser import get_config
 from deep_sort_pytorch.deep_sort import DeepSort
@@ -19,7 +27,7 @@ import cv2
 import torch
 import torch.backends.cudnn as cudnn
 import math
-
+import serial
 
 def compute_color_for_id(label):
     """
@@ -56,17 +64,18 @@ def dist_calculator(startX, startY, endX, endY, img_w, img_h):
     print(f'angle_left = {angle_left} + angle_right = {angle_right}')
 
     person_length = 800
+    if total_angle ==0:
+        return print("no person")
+    else:
+        distance = (person_length * (1.0 / total_angle) * 57) / 1000
 
-    distance = (person_length * (1.0 / total_angle) * 57) / 1000
 
     return distance
 
-
-
 def detect(opt):
-    out, source, yolo_weights, deep_sort_weights, show_vid, save_vid, save_txt, imgsz, evaluate = \
+    out, source, yolo_weights, deep_sort_weights, show_vid, save_vid, save_txt, imgsz, evaluate, half = \
         opt.output, opt.source, opt.yolo_weights, opt.deep_sort_weights, opt.show_vid, opt.save_vid, \
-            opt.save_txt, opt.img_size, opt.evaluate
+            opt.save_txt, opt.img_size, opt.evaluate, opt.half
     webcam = source == '0' or source.startswith(
         'rtsp') or source.startswith('http') or source.endswith('.txt')
 
@@ -76,12 +85,13 @@ def detect(opt):
     attempt_download(deep_sort_weights, repo='mikel-brostrom/Yolov5_DeepSort_Pytorch')
     deepsort = DeepSort(cfg.DEEPSORT.REID_CKPT,
                         max_dist=cfg.DEEPSORT.MAX_DIST, min_confidence=cfg.DEEPSORT.MIN_CONFIDENCE,
-                        nms_max_overlap=cfg.DEEPSORT.NMS_MAX_OVERLAP, max_iou_distance=cfg.DEEPSORT.MAX_IOU_DISTANCE,
+                        max_iou_distance=cfg.DEEPSORT.MAX_IOU_DISTANCE,
                         max_age=cfg.DEEPSORT.MAX_AGE, n_init=cfg.DEEPSORT.N_INIT, nn_budget=cfg.DEEPSORT.NN_BUDGET,
                         use_cuda=True)
 
     # Initialize
     device = select_device(opt.device)
+    half &= device.type != 'cpu'  # half precision only supported on CUDA
 
     # The MOT16 evaluation runs multiple inference streams in parallel, each one writing to
     # its own .txt file. Hence, in that case, the output folder is not restored
@@ -90,7 +100,6 @@ def detect(opt):
             pass
             shutil.rmtree(out)  # delete output folder
         os.makedirs(out)  # make new output folder
-    half = device.type != 'cpu'  # half precision only supported on CUDA
 
     # Load model
     model = attempt_load(yolo_weights, map_location=device)  # load FP32 model
@@ -110,7 +119,7 @@ def detect(opt):
         cudnn.benchmark = True  # set True to speed up constant image size inference
         dataset = LoadStreams(source, img_size=imgsz, stride=stride)
     else:
-        dataset = LoadImages(source, img_size=imgsz)
+        dataset = LoadImages(source, img_size=imgsz, stride=stride)
 
     # Get names and colors
     names = model.module.names if hasattr(model, 'module') else model.names
@@ -125,6 +134,7 @@ def detect(opt):
     txt_file_name = source.split('/')[-1].split('.')[0]
     txt_path = str(Path(out)) + '/' + txt_file_name + '.txt'
 
+
     for frame_idx, (path, img, im0s, vid_cap) in enumerate(dataset):
         img = torch.from_numpy(img).to(device)
         img = img.half() if half else img.float()  # uint8 to fp16/32
@@ -133,13 +143,13 @@ def detect(opt):
             img = img.unsqueeze(0)
 
         # Inference
-        t1 = time_synchronized()
+        t1 = time_sync()
         pred = model(img, augment=opt.augment)[0]
 
         # Apply NMS
         pred = non_max_suppression(
             pred, opt.conf_thres, opt.iou_thres, classes=opt.classes, agnostic=opt.agnostic_nms)
-        t2 = time_synchronized()
+        t2 = time_sync()
 
         # Process detections
         for i, det in enumerate(pred):  # detections per image
@@ -151,6 +161,7 @@ def detect(opt):
             s += '%gx%g ' % img.shape[2:]  # print string
             save_path = str(Path(out) / Path(p).name)
 
+
             if det is not None and len(det):
                 # Rescale boxes from img_size to im0 size
                 det[:, :4] = scale_coords(
@@ -159,22 +170,30 @@ def detect(opt):
                 # Print results
                 for c in det[:, -1].unique():
                     n = (det[:, -1] == c).sum()  # detections per class
-                    s += '%g %ss, ' % (n, names[int(c)])  # add to string
+                    s += f"{n} {names[int(c)]}{'s' * (n > 1)}, "  # add to string
 
                 xywhs = xyxy2xywh(det[:, 0:4])
                 confs = det[:, 4]
                 clss = det[:, 5]
 
                 # pass detections to deepsort
-                outputs = deepsort.update(xywhs.cpu(), confs.cpu(), clss, im0)
-
+                outputs = deepsort.update(xywhs.cpu(), confs.cpu(), clss.cpu(), im0)
+                
                 # draw boxes for visualization
                 if len(outputs) > 0:
                     for j, (output, conf) in enumerate(zip(outputs, confs)):
+                        bbox_left = output[0]
+                        bbox_top = output[1]
+                        bbox_w = output[2] - output[0]
+                        bbox_h = output[3] - output[1]
+                        center=[0,0]
+                        center_x = bbox_left + (bbox_w / 2)
+                        center_y = bbox_top + (bbox_h / 2)
 
                         dist = dist_calculator(startX=output[0], startY=output[1], endX=output[2], endY=output[3], img_w=640, img_h=480)
-                        print(f'dist = {dist}, left = {output[0]}, right = {output[2]}, top = {output[3]}, bottom = {output[1]}')
-                        print(f'output =  {output}')
+
+
+                        print(f'dist = {dist}')
                         bboxes = output[0:4]
                         id = output[4]
                         cls = output[5]
@@ -186,15 +205,27 @@ def detect(opt):
 
                         if save_txt:
                             if id == 1:
-                                # to MOT format
-                                bbox_left = output[0]
-                                bbox_top = output[1]
-                                bbox_w = output[2] - output[0]
-                                bbox_h = output[3] - output[1]
-                                # Write MOT compliant results to file
                                 with open(txt_path, 'a') as f:
-                                   f.write(('%g ' * 12 + '\n') % (frame_idx, id, dist, bbox_left,
-                                                               bbox_top, bbox_w, bbox_h, -1, -1, -1, -1, -1))  # label format
+                                    f.write(('%s ' * 1 + '\n') % (dist))  # label format
+
+                        ARD = serial.Serial('/dev/ttyACM0', 9600)
+                        if id == 1:
+                            if center_x < 180:
+                                c = "2"
+                                data = c.encode('utf-8')
+                                ARD.write(data)  # a값 전송
+                            if center_x > 480:
+                                c = "1"
+                                data = c.encode('utf-8')
+                                ARD.write(data)  # a값 전송
+                            if dist < 2.0:
+                                c = "3"
+                                data = c.encode('utf-8')
+                                ARD.write(data)
+                            else:
+                                c = "0"
+                                data = c.encode('utf-8')
+                                ARD.write(data)  # a값 전송
 
             else:
                 deepsort.increment_ages()
@@ -203,6 +234,7 @@ def detect(opt):
             print('%sDone. (%.3fs)' % (s, t2 - t1))
 
             # Stream results
+
             if show_vid:
                 cv2.imshow(p, im0)
                 if cv2.waitKey(1) == ord('q'):  # q to quit
@@ -224,6 +256,7 @@ def detect(opt):
 
                     vid_writer = cv2.VideoWriter(save_path, cv2.VideoWriter_fourcc(*'mp4v'), fps, (w, h))
                 vid_writer.write(im0)
+    # Set a PORT Number & baud rate
 
     if save_txt or save_vid:
         print('Results saved to %s' % os.getcwd() + os.sep + out)
@@ -233,9 +266,10 @@ def detect(opt):
     print('Done. (%.3fs)' % (time.time() - t0))
 
 
+
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
-    parser.add_argument('--yolo_weights', type=str, default='yolov5/weights/yolov5s.pt', help='model.pt path')
+    parser.add_argument('--yolo_weights', nargs='+', type=str, default='yolov5/weights/yolov5l.pt', help='model.pt path(s)')
     parser.add_argument('--deep_sort_weights', type=str, default='deep_sort_pytorch/deep_sort/deep/checkpoint/ckpt.t7', help='ckpt.t7 path')
     # file/folder, 0 for webcam
     parser.add_argument('--source', type=str, default='0', help='source')
@@ -254,6 +288,7 @@ if __name__ == '__main__':
     parser.add_argument('--augment', action='store_true', help='augmented inference')
     parser.add_argument('--evaluate', action='store_true', help='augmented inference')
     parser.add_argument("--config_deepsort", type=str, default="deep_sort_pytorch/configs/deep_sort.yaml")
+    parser.add_argument("--half", action="store_true", help="use FP16 half-precision inference")
     args = parser.parse_args()
     args.img_size = check_img_size(args.img_size)
 
